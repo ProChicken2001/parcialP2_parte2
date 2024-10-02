62dbe22a5dded4294b00e8b4922b49f2e5344860
Dennis Alexander Perez Urquiza <ia.pu100321@ufg.edu.sv>
2024-10-01 20:29:34 -0600

    add location page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,9 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
-
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+    implementation(libs.google.maps.android)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/sv/edu/ufg/fis/amb/parcialp2_parte2/pages/LocationPage.kt
+++ app/src/main/java/com/sv/edu/ufg/fis/amb/parcialp2_parte2/pages/LocationPage.kt
@@ -1,32 +1,64 @@
 package com.sv.edu.ufg.fis.amb.parcialp2_parte2.pages
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.ktx.model.cameraPosition
 import com.sv.edu.ufg.fis.amb.parcialp2_parte2.R
 import com.sv.edu.ufg.fis.amb.parcialp2_parte2.custom.components.AppBarIconButtonP
 import com.sv.edu.ufg.fis.amb.parcialp2_parte2.custom.components.AppBarIconButtonV
@@ -54,9 +86,10 @@ fun LocationPage(
                     bottom = innerPadding.calculateBottomPadding()
                 )
                 .fillMaxSize()
-                .background(color = colorResource(R.color.background))
+                .background(color = colorResource(R.color.background)),
+            verticalArrangement = Arrangement.Center
         ) {
-
+            BodyLocation(context = context, navController)
         }
     }
 }
@@ -147,4 +180,145 @@ fun LocationBottomAppBar(
             }
         }
     )
+}
+
+//---------------------------------------------------------------- [BODY LOCATION]
+
+@Composable
+fun BodyLocation(
+    context: Context,
+    navController: NavHostController
+){
+
+    var ubicacionPermiso by remember { mutableStateOf(false) }
+
+    val ubicacionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) {
+            permisos ->
+        ubicacionPermiso = permisos[Manifest.permission.ACCESS_FINE_LOCATION] == true ||
+                permisos[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+    }
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        colors = CardDefaults
+            .cardColors(containerColor = colorResource(R.color.background)),
+    ){
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            if(ubicacionPermiso){
+                MapLocation(context)
+            }else{
+                Image(
+                    modifier = Modifier
+                        .height(262.5.dp)
+                        .width(262.5.dp)
+                        .clip(RoundedCornerShape(25.dp)),
+                    painter = painterResource(R.drawable.location),
+                    contentDescription = "Image",
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 15.dp, end = 15.dp),
+                    text = "No se tienen permisos para acceder a la ubicacion",
+                    textAlign = TextAlign.Center,
+                )
+                Spacer(modifier = Modifier.height(7.5.dp))
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth(0.70f),
+                    onClick = {
+                        ubicacionLauncher.launch(
+                            arrayOf(
+                                Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION,
+                            )
+                        )
+                    }
+                ) {
+                    Text(
+                        "Acceder a la ubicacion",
+                        fontSize = 20.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MapLocation(
+    context: Context
+){
+    val ubicacionClient = LocationServices.getFusedLocationProviderClient(context)
+
+    var defaultLat = LatLng(13.794185, -88.89653)
+
+    val posicionCamara = rememberCameraPositionState{
+        position = CameraPosition.fromLatLngZoom(defaultLat, 12f)
+    }
+
+    if(ContextCompat
+        .checkSelfPermission(
+            context, Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED ||
+        ContextCompat.checkSelfPermission(
+            context, Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    ){
+        try{
+            ubicacionClient.lastLocation.addOnSuccessListener {
+                ubicacion ->
+                if(ubicacion != null){
+                    defaultLat = LatLng(ubicacion.latitude, ubicacion.longitude)
+                    posicionCamara.position = CameraPosition.fromLatLngZoom(
+                        LatLng(ubicacion.latitude, ubicacion.longitude), 15f
+                    )
+                    Toast
+                        .makeText(
+                            context,
+                            "Ubicacion actual en el mapa",
+                            Toast.LENGTH_LONG)
+                        .show()
+                }else{
+                    Toast
+                        .makeText(
+                            context,
+                            "No se pudo acceder a la ubicacion",
+                            Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }catch (ex : Exception){
+            Toast
+                .makeText(context, "Error al acceder a la ubicacion", Toast.LENGTH_LONG)
+                .show()
+        }
+
+    }else{
+        Toast
+            .makeText(context, "Ha ocurrido un error con los permisos", Toast.LENGTH_LONG)
+            .show()
+    }
+
+    GoogleMap(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.70f),
+        cameraPositionState = posicionCamara
+    ){
+        Marker(
+            state = MarkerState(position = defaultLat),
+            title = "Ubicacion Actual",
+            snippet = "Ubicacion tomada desde tu dispositivo",
+        )
+    }
 }
